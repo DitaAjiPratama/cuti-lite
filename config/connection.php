26587b2d2816775816c5723a550de5cf6d8f631e
Dita Aji Pratama <dita.aji.pratama@gmail.com>
2018-06-09 14:16:20 +0700

    Ganti Config

--- config/connection.php
+++ config/connection.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "cuti_lite";
+$database = "cuti";
 
 $con = mysqli_connect($host, $username, $password) or die("Cannot connect");
 mysqli_select_db($con, $database) or die("Cannot select database");
